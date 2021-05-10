f19b75a2f795418acdbbbfae53cfbb19882a04bd
Purbasha <purba91@ymail.com>
2021-05-10 19:39:36 +0530

    ok

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,7 +14,7 @@ class Controller extends BaseController
 	public function __construct(Request $objRequest)
     {
         define('AUTH_KEY', env('JWT_SECRET'));
-        define('STORAGE_URL', env('STORAGE_URL'));
+       
         
             if ($objRequest->header('Authorization')) {
                 if(AUTH_KEY!=$objRequest->header('Authorization')){
@@ -32,27 +32,5 @@ class Controller extends BaseController
      
     }
     
-    protected function respondWithToken($token)
-    {
-        return response()->json([
-            'token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => Auth::factory()->getTTL() * 60
-        ], 200);
-    }
-    protected function crypto_rand_secure($min, $max)
-    {
-        $range = $max - $min;
-        if ($range < 1) return $min; // not so random...
-        $log = ceil(log($range, 2));
-        $bytes = (int) ($log / 8) + 1; // length in bytes
-        $bits = (int) $log + 1; // length in bits
-        $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
-        do {
-            $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
-            $rnd = $rnd & $filter; // discard irrelevant bits
-        } while ($rnd > $range);
-        return $min + $rnd;
-    }
   
 }
